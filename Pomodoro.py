4d49e4235cc9e4adc22e0d89d4f4ae2aab754b63
Theo Martin Santoso <79010034+theomartinsantoso@users.noreply.github.com>
2021-05-06 10:34:01 +0700

    Create Pomodoro.py

---
+++ Pomodoro.py
@@ -0,0 +1,14 @@
+import time
+
+def countdownTimer(start_minute, start_second):
+    total_second = start_minute * 60 + start_second
+    while total_second:
+        mins, secs = divmod(total_second, 60)
+        print(f'{mins:02d}:{secs:02d}', end='\r')
+        time.sleep(1)
+        total_second -= 1
+    print("Done!")
+    
+
+if __name__ == '__main__':
+    countdownTimer(25, 00) #pomodoro timer
